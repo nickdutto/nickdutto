6a371c50c581e83d7d4f473a6ec5f36f25e48aa0
Nick <delta2888@gmail.com>
2023-08-17 13:36:59 +1000

    build: add next-themes dependency & ThemeProvider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import '~/styles/globals.css';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 
+import ThemeProvider from '~/components/ThemeProvider';
+
 const poppins = Poppins({
     subsets: ['latin'],
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -18,8 +20,12 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <html lang="en">
-            <body className={poppins.className}>{children}</body>
+        <html lang="en" suppressHydrationWarning>
+            <body className={poppins.className}>
+                <ThemeProvider attribute="class" defaultTheme="dark">
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 };
