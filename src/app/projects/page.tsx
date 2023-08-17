49418e2b68773fb45ee4d70a4d202d82b0ee678f
Nick <delta2888@gmail.com>
2023-08-17 15:01:50 +1000

    feat: add About, Project & Contact pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,13 @@
 import PortfolioCard from '~/components/PortfolioCard';
 
-const About = () => {
+const Projects = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <PortfolioCard>
-        <h1 className="text-3xl font-bold text-white">About</h1>
+        <h1 className="text-3xl font-bold text-white">Projects</h1>
       </PortfolioCard>
     </main>
   );
 };
 
-export default About;
+export default Projects;
