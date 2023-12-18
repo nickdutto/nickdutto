3eb112cb04c7ec8368ce6a0bee528aab7a578540
Nick <delta2888@gmail.com>
2023-12-18 13:38:39 +1100

    build: :heavy_minus_sign: remove next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
 import { type Metadata } from 'next';
 
-import ThemeProvider from '~/components/ThemeProvider';
-
 export const metadata: Metadata = {
   title: 'Nick Dutton',
   description: 'Fullstack Web Developer, Australia',
@@ -18,9 +16,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning>
       <body className='bg-topography bg-black'>
-        <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
-        </ThemeProvider>
       </body>
     </html>
   );
