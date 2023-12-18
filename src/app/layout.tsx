f903745d92ad137c854420ae234162e51c9fefdc
Nick <delta2888@gmail.com>
2023-12-18 13:34:18 +1100

    build: :heavy_plus_sign: add geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import '~/styles/globals.css';
 
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
 import { type Metadata } from 'next';
-import { Poppins } from 'next/font/google';
 
 import ThemeProvider from '~/components/ThemeProvider';
-import { cn } from '~/lib/utils';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-});
 
 export const metadata: Metadata = {
   title: 'Nick Dutton',
@@ -21,8 +16,8 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={cn(poppins.className, 'bg-topography')}>
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`} suppressHydrationWarning>
+      <body className='bg-topography bg-black'>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
