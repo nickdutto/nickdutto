f83fb9f68d9ce9ae31f28ca9de09efc85286402a
Nick <delta2888@gmail.com>
2024-02-01 17:09:24 +1100

    build: :heavy_plus_sign: add sonner

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -3,6 +3,7 @@
 import { NextUIProvider } from '@nextui-org/react';
 
 import { useRouter } from 'next/navigation';
+import { Toaster } from 'sonner';
 
 type Props = {
   children: React.ReactNode;
@@ -11,7 +12,12 @@ type Props = {
 const Providers = ({ children }: Props) => {
   const router = useRouter();
 
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider navigate={router.push}>
+      {children}
+      <Toaster />
+    </NextUIProvider>
+  );
 };
 
 export default Providers;
